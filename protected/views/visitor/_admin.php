77e2c3d91ffd448fcf89b97305f72c8881efc23c
Jeremiah <jeremiah.pajarillo@stableapps.com>
2015-01-28 11:26:23 +0800

    Removed flash messages in admin view

--- protected/views/visitor/_admin.php
+++ protected/views/visitor/_admin.php
@@ -5,17 +5,7 @@
 ?>
 
 <h1>Manage Visitors</h1>
-<?php if(Yii::app()->user->hasFlash('success')):?>
-    <div class="flash-success">
-        <?php echo Yii::app()->user->getFlash('success'); ?>
-    </div>
-<?php endif; ?>
- 
-<?php if(Yii::app()->user->hasFlash('error')):?>
-    <div class="flash-error">
-        <?php echo Yii::app()->user->getFlash('error'); ?>
-    </div>
-<?php endif; ?>
+
 <?php
 $this->widget('zii.widgets.grid.CGridView', array(
     'id' => 'visitor-grid',

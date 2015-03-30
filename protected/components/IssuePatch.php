2745a01d8e78d54c5957438e30dec5ace1b69e98
Jeremiah <jeremiah.pajarillo@stableapps.com>
2015-03-30 12:35:28 +0800

    #137 - fixed issue patch -> added condition if patch was already implemented

--- protected/components/IssuePatch.php
+++ protected/components/IssuePatch.php
@@ -12,67 +12,74 @@ class IssuePatch extends CComponent {
             /* update */
             $checkIfColumnExists = $db->createCommand("SHOW COLUMNS FROM `card_generated` LIKE 'company_code'");
             $result = $checkIfColumnExists->query();
-
-            if ($result->rowCount == 0) {
+            
+            $checkIfColumnExists2 = $db->createCommand("SHOW COLUMNS FROM `card_generated` LIKE 'card_number'");
+            $result2 = $checkIfColumnExists2->query();
+            
+            if ($result->rowCount == 0 && $result->rowCount != 0) {
                 $sql = 'ALTER TABLE `card_generated` DROP `card_code`; 
                     ALTER TABLE `card_generated` ADD COLUMN `company_code` VARCHAR(3) DEFAULT 0 NULL AFTER `tenant_agent`;
                     ALTER TABLE `card_generated` ADD COLUMN `card_count` BIGINT DEFAULT 0 NULL AFTER `company_code`;
                     ALTER TABLE `card_generated` ADD COLUMN `print_count` BIGINT DEFAULT 0 NULL AFTER `card_count`;
                     ';
                 $db->createCommand($sql)->execute();
-            }
 
-            $command = $db->createCommand("select id,tenant,company_code,card_count from card_generated");
-            $result = $command->queryAll();
+                $command = $db->createCommand("select id,tenant,company_code,card_count from card_generated");
+                $result = $command->queryAll();
 
-            foreach ($result as $row) {
-                $sql = 'update card_generated set '
-                        . 'company_code=(select `code` from company '
-                        . 'where id=(select company from `user` where id="' . $row['tenant'] . '")) where'
-                        . ' tenant = "' . $row['tenant'] . '"';
-                $db->createCommand($sql)->execute();
-                $command = $db->createCommand('select MAX(card_count)+1 as max from card_generated where tenant="' . $row['tenant'] . '"');
-                $max = $command->query();
-                foreach ($max as $maxRow) {
+                foreach ($result as $row) {
+                    $sql = 'update card_generated set '
+                            . 'company_code=(select `code` from company '
+                            . 'where id=(select company from `user` where id="' . $row['tenant'] . '")) where'
+                            . ' tenant = "' . $row['tenant'] . '"';
+                    $db->createCommand($sql)->execute();
+                    $command = $db->createCommand('select MAX(card_count)+1 as max from card_generated where tenant="' . $row['tenant'] . '"');
+                    $max = $command->query();
+                    foreach ($max as $maxRow) {
+                        $sql = 'update card_generated set card_count="' . $maxRow['max'] . '" where id="' . $row['id'] . '"';
+                        $db->createCommand($sql)->execute();
+                    }
+
                     $sql = 'update card_generated set card_count="' . $maxRow['max'] . '" where id="' . $row['id'] . '"';
                     $db->createCommand($sql)->execute();
                 }
 
-                $sql = 'update card_generated set card_count="' . $maxRow['max'] . '" where id="' . $row['id'] . '"';
-                $db->createCommand($sql)->execute();
-            }
+                $commandA = $db->createCommand("select id,visit_status,visitor,tenant,tenant_agent from visit where (visit_status=1 or visit_status=3) and (card IS NULL or card ='')");
+                $resultA = $commandA->queryAll();
+                foreach ($resultA as $key => $value) {
+                    $commandB = $db->createCommand("select code from company where id=(Select company from user where id='" . $value['tenant'] . "')");
+                    $resultB = $commandB->queryAll();
+                    foreach ($resultB as $key => $valueB) {
+                        $card_code = $valueB['code'];
+                    }
 
-            $commandA = $db->createCommand("select id,visit_status,visitor,tenant,tenant_agent from visit where (visit_status=1 or visit_status=3) and (card IS NULL or card ='')");
-            $resultA = $commandA->queryAll();
-            foreach ($resultA as $key => $value) {
-                $commandB = $db->createCommand("select code from company where id=(Select company from user where id='" . $value['tenant'] . "')");
-                $resultB = $commandB->queryAll();
-                foreach ($resultB as $key => $valueB) {
-                    $card_code = $valueB['code'];
-                }
-
-                $commandC = $db->createCommand("select max(card_count) + 1 as max from card_generated where tenant='" . $value['tenant'] . "'");
-                $resultC = $commandC->queryAll();
-                foreach ($resultC as $key => $valueC) {
-                    $card_count = $valueC['max'];
-                }
-                $t_agent = ',NULL,';
-                if ($value['tenant_agent'] != '') {
-                    $t_agent = ',"' . $value['tenant_agent'] . '",';
-                }
-                $sqlA = 'INSERT INTO `card_generated` (`date_printed`,`date_expiration`, `date_cancelled`, `date_returned`, 
+                    $commandC = $db->createCommand("select max(card_count) + 1 as max from card_generated where tenant='" . $value['tenant'] . "'");
+                    $resultC = $commandC->queryAll();
+                    foreach ($resultC as $key => $valueC) {
+                        $card_count = $valueC['max'];
+                    }
+                    $t_agent = ',NULL,';
+                    if ($value['tenant_agent'] != '') {
+                        $t_agent = ',"' . $value['tenant_agent'] . '",';
+                    }
+                    $sqlA = 'INSERT INTO `card_generated` (`date_printed`,`date_expiration`, `date_cancelled`, `date_returned`, 
                         `card_image_generated_filename`, `visitor_id`, `card_status`, 
                         `created_by`, `tenant`, `tenant_agent`, `company_code`, 
                         `card_count`, `print_count`) VALUES (NULL, NULL, 
                         NULL, NULL, NULL, "' . $value['visitor'] . '", 
                         NULL, "' . $value['tenant'] . '",  "' . $value['tenant'] . '"
                         ' . $t_agent . ' "' . $card_code . '", "' . $card_count . '", "0")';
-                $db->createCommand($sqlA)->execute();
+                    $db->createCommand($sqlA)->execute();
 
-                $sqlB = 'update visit set card="' . Yii::app()->db->getLastInsertID() . '" where id="' . $value['id'] . '"';
-                $db->createCommand($sqlB)->execute();
+                    $sqlB = 'update visit set card="' . Yii::app()->db->getLastInsertID() . '" where id="' . $value['id'] . '"';
+                    $db->createCommand($sqlB)->execute();
+                }
+                echo "<br>Done patch for issue81";
+            } else {
+                echo "<br>Issue 81 Already patched.";
             }
-            echo "<br>Done patch for issue81";
+
+            
             return true;
         } catch (Exception $ex) {
             echo 'ERROR IN PATCH 81 PATCHER';
@@ -86,40 +93,45 @@ class IssuePatch extends CComponent {
             /* update */
             $checkIfColumnExists = $db->createCommand("SHOW COLUMNS FROM `company` LIKE 'card_number'");
             $result = $checkIfColumnExists->query();
+            
+            $checkIfColumnExists2 = $db->createCommand("SHOW COLUMNS FROM `card_generated` LIKE 'card_count'");
+            $result2 = $checkIfColumnExists2->query();
+            
 
-            if ($result->rowCount == 0) {
+            if ($result->rowCount == 0 && $result2->rowCount != 0) {
                 $sql = 'ALTER TABLE `card_generated` ADD COLUMN `card_number` VARCHAR(10) NULL AFTER `id`; 
                     ALTER TABLE `company` ADD COLUMN `card_count` BIGINT NULL AFTER `is_deleted`; 
                     ';
                 $db->createCommand($sql)->execute();
-            }
 
+                $command = $db->createCommand("select * from card_generated");
+                $result = $command->queryAll();
 
-            $command = $db->createCommand("select * from card_generated");
-            $result = $command->queryAll();
-
-            foreach ($result as $row) {
-                $sql = 'update card_generated set '
-                        . 'card_number="' . IssuePatch::generateCardNumber($row['tenant'], $row['card_count']) . '" where'
-                        . ' id = "' . $row['id'] . '"';
-                $db->createCommand($sql)->execute();
-
-                $command = $db->createCommand('select MAX(card_count) as max from card_generated where tenant="' . $row['tenant'] . '"');
-                $max = $command->query();
-
-                foreach ($max as $maxRow) {
-                    $sql = 'update company set card_count="' . $maxRow['max'] . '" where id=(select company from `user`'
-                            . 'where id="' . $row['tenant'] . '")';
+                foreach ($result as $row) {
+                    $sql = 'update card_generated set '
+                            . 'card_number="' . IssuePatch::generateCardNumber($row['tenant'], $row['card_count']) . '" where'
+                            . ' id = "' . $row['id'] . '"';
                     $db->createCommand($sql)->execute();
-                }
-            }
-            $sql = 'ALTER TABLE `card_generated` DROP COLUMN `company_code`, DROP COLUMN `card_count`; ';
-            $db->createCommand($sql)->execute();
 
-            echo "<br>Done patch for issue137";
+                    $command = $db->createCommand('select MAX(card_count) as max from card_generated where tenant="' . $row['tenant'] . '"');
+                    $max = $command->query();
+
+                    foreach ($max as $maxRow) {
+                        $sql = 'update company set card_count="' . $maxRow['max'] . '" where id=(select company from `user`'
+                                . 'where id="' . $row['tenant'] . '")';
+                        $db->createCommand($sql)->execute();
+                    }
+                }
+                $sql = 'ALTER TABLE `card_generated` DROP COLUMN `company_code`, DROP COLUMN `card_count`; ';
+                $db->createCommand($sql)->execute();
+                echo "<br>Done patch for issue137";
+            } else {
+                echo "<br>Issue 137 Already patched.";
+            }
+
             return true;
         } catch (Exception $ex) {
-            echo 'ERROR IN PATCH 137 PATCHER';
+            echo ' ERROR IN PATCH 137 PATCHER';
             return false;
         }
     }

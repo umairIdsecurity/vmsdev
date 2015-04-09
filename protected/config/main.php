090fa28dc39a9551cc0956952aebcacbd0cf3bce
Yuriv <yuriy.vodolazsky@austechsystems.com>
2015-04-09 12:54:51 +0300

    Accidentally changed db settings restored

--- protected/config/main.php
+++ protected/config/main.php
@@ -76,8 +76,8 @@ return array(
          */
         'db' => array(
             'connectionString' => 'mysql:host=localhost;dbname=vms',
-            'username' => 'root',
-            'password' => 'mysql',
+            'username' => 'user_vms',
+            'password' => 'HFz7c9dHrmPqwNGr',
             'charset' => 'utf8',
             'enableParamLogging' => true,
             'enableProfiling' => true,

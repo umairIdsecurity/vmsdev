95d3e4504bf02ab0b47253f15529b3f3878d0f68
Sadaf Rasheed <sadaf@discretelogix.com>
2015-05-02 00:22:32 +0500

    Fixed Link, as specified in Issue#302

--- protected/views/user/_form.php
+++ protected/views/user/_form.php
@@ -430,7 +430,8 @@ echo '<h1>Add User </h1>';
                 <tr>
                 <td align="center">
                     <div class="row buttons" style="margin-left:23.5px;">
-                    <?php if(!is_null($companyLafPreferences)){$company_neutral_bg_color = $companyLafPreferences->neutral_bg_color;} ?>
+                    <?php $company_neutral_bg_color = "";
+                    if(!is_null($companyLafPreferences)){$company_neutral_bg_color = "background: ".$companyLafPreferences->neutral_bg_color;} ?>
                     <input onclick="generatepassword();" class="complete btn btn-info" style="position: relative; width:178px; overflow: hidden; cursor: default;background:<?php echo $company_neutral_bg_color ?> !important;cursor:pointer;font-size:14px" type="button" value="Autogenerate Password" />
                         
                     </div>

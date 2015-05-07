aff7e41651468a53d5d8f47b59ed1c25f2f1fbad
Faha Aliz <farhat.aziz27@hotmail.com>
2015-05-07 11:03:22 +0500

    issue 170 migration helpdesk

---
+++ protected/migrations/m150506_154747_helpdesk_group_tables.php
@@ -0,0 +1,69 @@
+<?php
+
+class m150506_154747_helpdesk_group_tables extends CDbMigration
+{
+	
+	public function safeUp()
+	{
+        $table = Yii::app()->db->schema->getTable('helpdesk_group');
+        if(!isset($table)){
+
+            $this->execute("
+
+                   CREATE TABLE IF NOT EXISTS `helpdesk_group` (
+                            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                            `name` varchar(255) NOT NULL,
+                            `order_by` int(6) DEFAULT NULL,
+							`created_by` bigint(20) DEFAULT NULL,
+							`is_deleted` bigint(1) DEFAULT 0,
+                            PRIMARY KEY (`id`)                             )
+            ");
+
+        }
+		
+		
+		$table = Yii::app()->db->schema->getTable('helpdesk');
+        if(!isset($table)){
+
+            $this->execute("
+
+                   CREATE TABLE IF NOT EXISTS `helpdesk` (
+                            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                            `question` varchar(255) NOT NULL,
+							`answer` text NOT NULL,
+                            `helpdesk_group_id` bigint(20) NOT NULL,
+							`order_by` int(6) DEFAULT NULL,
+							`created_by` bigint(20) DEFAULT NULL,
+							`is_deleted` int(1) DEFAULT 0,
+                            PRIMARY KEY (`id`),
+                            CONSTRAINT `helpdesk_helpdesk_group` FOREIGN KEY (`helpdesk_group_id`) REFERENCES `helpdesk_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE                             )
+            ");
+
+        }
+
+	}
+
+	public function safeDown()
+	{
+        $table = Yii::app()->db->schema->getTable('helpdesk_group');
+        if(isset($table)) {
+
+            $this->execute("DELETE FROM `helpdesk_group` ");
+
+            $this->execute("DROP TABLE `helpdesk_group` ");
+
+        }
+		
+		$table = Yii::app()->db->schema->getTable('helpdesk');
+        if(isset($table)) {
+
+            $this->execute("DELETE FROM `helpdesk` ");
+
+            $this->execute("DROP TABLE `helpdesk` ");
+
+        }
+
+      
+    }
+
+}

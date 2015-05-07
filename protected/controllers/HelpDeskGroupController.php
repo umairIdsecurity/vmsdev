98743e8a8176ac1e2f71e207308848792283ccf4
Faha Aliz <farhat.aziz27@hotmail.com>
2015-05-07 13:54:24 +0500

    issue 170

--- protected/controllers/HelpDeskGroupController.php
+++ protected/controllers/HelpDeskGroupController.php
@@ -44,10 +44,7 @@ class HelpDeskGroupController extends Controller {
 		
 		$table = Yii::app()->db->schema->getTable('helpdesk_group');
         if(!isset($table)){
-
-           echo "not set ";
-		   exit();
-           /* $this->execute("
+             $this->execute("
 
                    CREATE TABLE IF NOT EXISTS `helpdesk_group` (
                             `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -57,7 +54,23 @@ class HelpDeskGroupController extends Controller {
 							`is_deleted` bigint(1) DEFAULT 0,
                             PRIMARY KEY (`id`)                             )
             ");
-*/
+
+        }
+		$table = Yii::app()->db->schema->getTable('helpdesk');
+        if(!isset($table)){
+
+            $this->execute("CREATE TABLE IF NOT EXISTS `helpdesk` (
+                            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                            `question` varchar(255) NOT NULL,
+							`answer` text NOT NULL,
+                            `helpdesk_group_id` bigint(20) NOT NULL,
+							`order_by` int(6) DEFAULT NULL,
+							`created_by` bigint(20) DEFAULT NULL,
+							`is_deleted` int(1) DEFAULT 0,
+                            PRIMARY KEY (`id`),
+                            CONSTRAINT `helpdesk_helpdesk_group` FOREIGN KEY (`helpdesk_group_id`) REFERENCES `helpdesk_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE                             )
+            ");
+
         }
 		echo "set";
 		exit();

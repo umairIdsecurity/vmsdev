dc50c511f361e28429704418474343c028a9520b
spiritvoin <Spiritvoin88@gmail.com>
2015-05-13 18:00:36 +0300

    #223 Log out hover and add logo to Administration Menu complit

--- protected/views/user/admin_menu.php
+++ protected/views/user/admin_menu.php
@@ -17,7 +17,7 @@ if ($session['role'] == Roles::ROLE_AGENT_OPERATOR || $session['role'] == Roles:
     echo 'display:none;';
 }
 ?>" class="administrationMenu">
-    <div class="sidebarTitle" style=""><a href="#" class="dashboard-icon"></a>Administration</div><br><div id='cssmenu' >
+    <div class="sidebarTitle" style=""><a href="<?php echo Yii::app()->createUrl('workstation/admin') ?>" class="dashboard-icon"></a>Administration</div><br><div id='cssmenu' >
         <ul>
 
             <?php

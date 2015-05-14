d34d546ca5109fe7ba8dd3f501493ada7a335285
spiritvoin <Spiritvoin88@gmail.com>
2015-05-14 17:30:19 +0300

    #303 Duplicate Host Profiles in Manage Users code format

--- protected/migrations/m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users.php
+++ protected/migrations/m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users.php
@@ -10,9 +10,9 @@ class m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users extends C
         try {
             $COLUMNS = Yii::app()->db->createCommand()->select('COLUMN_NAME')->from('INFORMATION_SCHEMA.COLUMNS')->WHERE('table_name = \'user\' and TABLE_SCHEMA=\'' . $DBName . '\' and COLUMN_NAME!=\'password\' and COLUMN_NAME!=\'id\' and COLUMN_NAME!=\'is_deleted\'')->queryColumn();
 
-            $this->dropForeignKey('user_workstation_ibfk_3','user_workstation');
+            $this->dropForeignKey('user_workstation_ibfk_3', 'user_workstation');
             $this->addForeignKey('user_workstation_ibfk_3', 'user_workstation', 'user', 'user', 'id', 'CASCADE', 'CASCADE');
-            $this->dropForeignKey('visit_ibfk_6','visit');
+            $this->dropForeignKey('visit_ibfk_6', 'visit');
             $this->addForeignKey('visit_ibfk_6', 'visit', 'host', 'user', 'id', 'CASCADE', 'CASCADE');
 
             $ids = Yii::app()->db->createCommand()->select('MAX(id) as id ,COUNT(*) cnt, ' . implode(',', $COLUMNS))->from('user as u')->GROUP(implode(',', $COLUMNS))->HAVING('cnt > 1')->queryColumn();
@@ -30,10 +30,10 @@ class m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users extends C
     {
         try {
 
-           $this->dropForeignKey('user_workstation_ibfk_3','user_workstation');
-           $this->addForeignKey('user_workstation_ibfk_3', 'user_workstation', 'user', 'user', 'id', 'RESTRICT', 'RESTRICT');
-           $this->dropForeignKey('visit_ibfk_6','visit');
-           $this->addForeignKey('visit_ibfk_6', 'visit', 'host', 'user', 'id', 'RESTRICT', 'RESTRICT');
+            $this->dropForeignKey('user_workstation_ibfk_3', 'user_workstation');
+            $this->addForeignKey('user_workstation_ibfk_3', 'user_workstation', 'user', 'user', 'id', 'RESTRICT', 'RESTRICT');
+            $this->dropForeignKey('visit_ibfk_6', 'visit');
+            $this->addForeignKey('visit_ibfk_6', 'visit', 'host', 'user', 'id', 'RESTRICT', 'RESTRICT');
             return true;
         } catch (Exception $ex) {
             echo 'ERROR IN PATCH 303 PATCHER ' . $ex;

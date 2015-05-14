df6a744a203bb66c98c250284423d6d2905aa1be
spiritvoin <Spiritvoin88@gmail.com>
2015-05-14 16:57:14 +0300

    #303 Duplicate Host Profiles in Manage Users

--- protected/migrations/m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users.php
+++ protected/migrations/m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users.php
@@ -8,19 +8,12 @@ class m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users extends C
         $DBName = $this->getDBName();
 
         try {
-            $COLUMNS = Yii::app()->db->createCommand()->select('COLUMN_NAME')->from('INFORMATION_SCHEMA.COLUMNS')->WHERE('table_name = \'user\' and TABLE_SCHEMA=\'' . $DBName . '\' and COLUMN_NAME!=\'password\' and COLUMN_NAME!=\'id\'')->queryColumn();
+            $COLUMNS = Yii::app()->db->createCommand()->select('COLUMN_NAME')->from('INFORMATION_SCHEMA.COLUMNS')->WHERE('table_name = \'user\' and TABLE_SCHEMA=\'' . $DBName . '\' and COLUMN_NAME!=\'password\' and COLUMN_NAME!=\'id\' and COLUMN_NAME!=\'is_deleted\'')->queryColumn();
 
-            $this->execute(
-                "ALTER TABLE  user_workstation
- DROP FOREIGN KEY  user_workstation_ibfk_3 ;
-
- ALTER TABLE  user_workstation
- ADD CONSTRAINT  user_workstation_ibfk_3
- FOREIGN KEY (user)
- REFERENCES  {$DBName}.user (id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE ;"
-            );
+            $this->dropForeignKey('user_workstation_ibfk_3','user_workstation');
+            $this->addForeignKey('user_workstation_ibfk_3', 'user_workstation', 'user', 'user', 'id', 'CASCADE', 'CASCADE');
+            $this->dropForeignKey('visit_ibfk_6','visit');
+            $this->addForeignKey('visit_ibfk_6', 'visit', 'host', 'user', 'id', 'CASCADE', 'CASCADE');
 
             $ids = Yii::app()->db->createCommand()->select('MAX(id) as id ,COUNT(*) cnt, ' . implode(',', $COLUMNS))->from('user as u')->GROUP(implode(',', $COLUMNS))->HAVING('cnt > 1')->queryColumn();
             if (!empty($ids)) {
@@ -35,19 +28,12 @@ class m150512_111015_issue_303_Duplicate_Host_Profiles_in_Manage_Users extends C
 
     public function down()
     {
-        $DBName = $this->getDBName();
         try {
-            $this->execute(
-                "ALTER TABLE  user_workstation
-  DROP FOREIGN KEY  user_workstation_ibfk_3 ;
 
-ALTER TABLE  user_workstation
-ADD CONSTRAINT  user_workstation_ibfk_3
-FOREIGN KEY (  user )
-REFERENCES  {$DBName}.user (id)
- ON DELETE RESTRICT
-  ON UPDATE RESTRICT ;"
-            );
+           $this->dropForeignKey('user_workstation_ibfk_3','user_workstation');
+           $this->addForeignKey('user_workstation_ibfk_3', 'user_workstation', 'user', 'user', 'id', 'RESTRICT', 'RESTRICT');
+           $this->dropForeignKey('visit_ibfk_6','visit');
+           $this->addForeignKey('visit_ibfk_6', 'visit', 'host', 'user', 'id', 'RESTRICT', 'RESTRICT');
             return true;
         } catch (Exception $ex) {
             echo 'ERROR IN PATCH 303 PATCHER ' . $ex;

5cf6711c4f41018c203014b79a99502dd679e986
ntdat <ntdat@ceresolutions.com>
2015-05-20 18:06:18 +0700

    CAVMS-31: Select Visitor Profile type icons are not sensitive for the clicks

--- protected/views/visitor/profile_type.php
+++ protected/views/visitor/profile_type.php
@@ -3,22 +3,17 @@
   
             <input type="radio" value="<?php echo Visitor::PROFILE_TYPE_CORPORATE; ?>" id="<?php echo Visitor::PROFILE_TYPE_CORPORATE; ?>" name="Visitor[profile_type]" />
             <label for="<?php echo Visitor::PROFILE_TYPE_CORPORATE; ?>" >
-                <img src="<?php echo Yii::app()->controller->assetsBase . '/images/corporate-visitor-icon.png'; ?>"/>
                 <p class="corp">Corporate</p>
             </label>
  
             <input type="radio" value="<?php echo Visitor::PROFILE_TYPE_VIC; ?>" id="<?php echo Visitor::PROFILE_TYPE_VIC; ?>" name="Visitor[profile_type]" />
             <label for="<?php echo Visitor::PROFILE_TYPE_VIC; ?>" >
-                <img src="<?php echo Yii::app()->controller->assetsBase . '/images/vic-visitor-icon.png'; ?>"/>
                 <p class="vic">VIC</p>
             </label>
  
             <input type="radio" value="<?php echo Visitor::PROFILE_TYPE_ASIC; ?>" id="<?php echo Visitor::PROFILE_TYPE_ASIC; ?>" name="Visitor[profile_type]" />
             <label for="<?php echo Visitor::PROFILE_TYPE_ASIC; ?>" >
-                <img src="<?php echo Yii::app()->controller->assetsBase . '/images/asic-visitor-icon.png'; ?>"/>
                 <p class="asic">ASIC</p>
-            
-
             </label>
  
 </div>

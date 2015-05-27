8ac3f865b49a193a5e1e353a823dee3ef664e116
Tran Pham <tran.pham@ceresolutions.com>
2015-05-27 15:25:35 +0700

    CAVMS-216 - Default Password Options to User does not Require password

--- protected/views/common_partials/password.php
+++ protected/views/common_partials/password.php
@@ -14,7 +14,12 @@ $companyLafPreferences = CompanyLafPreferences::model()->findByPk($company->comp
         <table style="margin-top:18px !important; width:273px; border-left-style:none; border-top-style:none">
                 <tr>
                     <td>
-                        <?php echo $form->radioButtonList($model, 'password_requirement',
+                        <?php
+                        if (!isset($model->password_requirement)) {
+                            $model->password_requirement = PasswordRequirement::PASSWORD_IS_NOT_REQUIRED;
+                        }
+
+                        echo $form->radioButtonList($model, 'password_requirement',
                             array(
                                 PasswordRequirement::PASSWORD_IS_NOT_REQUIRED => 'User does not require Password',
                                 PasswordRequirement::PASSWORD_IS_REQUIRED     => 'User requires Password to Login',

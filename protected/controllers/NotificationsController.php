f7b3eac3bded510da0ade9776bea8963fd4732e6
dxasher <developer@discretelogix.com>
2015-05-28 09:49:17 +0500

    Notification Delete fixed

--- protected/controllers/NotificationsController.php
+++ protected/controllers/NotificationsController.php
@@ -140,10 +140,11 @@ class NotificationsController extends Controller
 	public function actionDelete($id)
 	{
 		$this->loadModel($id)->delete();
-
+                UserNotification::model()->find('notification_id = '.$id)->deleteAll();
+                
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+                    $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
 	}
 
 	/**

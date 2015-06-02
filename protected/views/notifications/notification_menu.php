368b08b351494205cf0be2cd77bcc3073a69a9e6
dxasher <developer@discretelogix.com>
2015-06-01 22:53:49 -0700

    companies were deleted so as their preferences hence no style were applying based on their preferences. Also notifications ' subjects fixed if less than 40 characters, do nothing else put ... after 40 characters.

--- protected/views/notifications/notification_menu.php
+++ protected/views/notifications/notification_menu.php
@@ -4,24 +4,24 @@ $company = Company::model()->findByPk($session['company']);
 if (isset($company) && !empty($company)) {
     $companyLafPreferences = CompanyLafPreferences::model()->findByPk($company->company_laf_preferences);
 }
-if (isset($companyLafPreferences) && !empty($companyLafPreferences)) {
+
 ?> 
 <style>
      .notification-dropdown li > h4 {
      padding: 6px 12px;
      color:#fff;
-     background: <?php echo $companyLafPreferences->nav_hover_color;?>;
+     background: <?php echo isset($companyLafPreferences) ? $companyLafPreferences->nav_hover_color : '#9BD62C' ;?>;
      margin: 0px !important;
      font-size:15px;
  } 
 </style>    
-<?php } ?>
+
 <ul class="dropdown-menu notification-dropdown">
     <li> <h4> Notifications </h4></li>
      <?php 
            if($notifications) 
             foreach($notifications as $key => $notify ) {
-     ?>   <li><a class="notification-a" href="<?php echo Yii::app()->createUrl("/notifications/view/id/{$notify->id}"); ?>" style="font-weight:bold !important;"> <?php echo substr($notify->subject,0, 35) ?>... 
+     ?>   <li><a class="notification-a" href="<?php echo Yii::app()->createUrl("/notifications/view/id/{$notify->id}"); ?>" style="font-weight:bold !important;"> <?php if(strlen($notify->subject) <=40 ) { echo $notify->subject; }else{ echo substr($notify->subject,0, 40) . '...'; }  ?> 
                 <span> <br> <?php echo $notify->notification_type?> <br> <?php echo $notify->date_created?> </span>  
                </a>
           </li>

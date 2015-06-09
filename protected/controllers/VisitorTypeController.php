48058deaf65fa7289ecb138f0c6d80acdd8ff0a9
Muhammad Asher <developer@discretelogix.com>
2015-06-09 16:28:22 -0700

    CVMS Reports: Total Visitors by Workstation

--- protected/controllers/VisitorTypeController.php
+++ protected/controllers/VisitorTypeController.php
@@ -26,7 +26,7 @@ class VisitorTypeController extends Controller {
     public function accessRules() {
         return array(
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('create', 'update', 'admin', 'delete','adminAjax', 'visitorsByTypeReport', 'index'),
+                'actions' => array('create', 'update', 'admin', 'delete','adminAjax','visitorsByWorkstationReport','visitorsByTypeReport', 'index'),
                 'expression' => 'UserGroup::isUserAMemberOfThisGroup(Yii::app()->user,UserGroup::USERGROUP_ADMINISTRATION)',
            
             ),
@@ -187,5 +187,43 @@ class VisitorTypeController extends Controller {
 
         $this->render("visitortypecount", array("visit_count"=>$visitsCount));
     } 
-
+    
+    /* 
+    * Report: Corporate Reporting: Total Visitors by Workstations
+    * Total Visitors by Workstations
+    * 
+    * @return view
+    */
+    public function actionVisitorsByWorkstationReport() {
+        // Post Date
+        $dateFromFilter = Yii::app()->request->getParam("date_from_filter");
+        $dateToFilter = Yii::app()->request->getParam("date_to_filter");
+        
+        $dateCondition='';
+        
+        if( !empty($dateFromFilter) && !empty($dateToFilter) ) {
+            
+            $from = new DateTime($dateFromFilter);
+            $to = new DateTime($dateToFilter);
+            
+             $dateCondition = '(visits.date_check_in BETWEEN "'.$from->format('d-m-Y').'"'
+                                . ' AND "'.$to->format('d-m-Y').'") OR (visits.date_check_in BETWEEN "'.$from->format('Y-m-d').'"'
+                                . ' AND "'.$to->format('Y-m-d').'") AND';
+        
+        }
+        
+        $dateCondition .= '(t.is_deleted = 0) AND (visits.is_deleted = 0) AND (visitors.is_deleted = 0)';
+        
+        
+        $visitors = Yii::app()->db->createCommand()
+                    ->select('min(visits.id) as visitId,visits.date_check_in as date_check_in,t.id,t.name,count(visitors.id) as visitors')
+                    ->from('workstation t')
+                    ->leftJoin('visitor visitors' , '(t.id = visitors.visitor_workstation)')
+                    ->leftJoin('visit visits' , '(visits.visitor = visitors.id)')
+                    ->where($dateCondition)
+                    ->group('t.id')
+                    ->queryAll(); // this will be returned as an array of arrays
+        
+        $this->render("visitorbyworkstationcount", array("visitor_count"=>$visitors));
+    }
 }

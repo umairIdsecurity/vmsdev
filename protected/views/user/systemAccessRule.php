e8e1f472c010c8e8da2f418e56621d9a66b51375
Muhammad Asher <developer@discretelogix.com>
2015-06-08 18:51:30 -0700

    new changes in administrator access rules gui

--- protected/views/user/systemAccessRule.php
+++ protected/views/user/systemAccessRule.php
@@ -60,7 +60,7 @@ $this->widget('zii.widgets.grid.CGridView', array(
 ?>
 
 
-<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id' => 'workstationsModal','htmlOptions'=>array('style' => 'height:75%'))); ?>
+<?php $this->beginWidget('bootstrap.widgets.TbModal', array('id' => 'workstationsModal')); ?>
 
 <div class="modal-header">
     <a class="close" data-dismiss="modal">&times;</a>

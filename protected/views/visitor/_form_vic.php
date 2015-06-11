6af5881409de8a31b648e22db4a9f2388ddec307
Thao Tran <ththao@ceresolutions.com>
2015-06-11 14:34:42 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-411

--- protected/views/visitor/_form_vic.php
+++ protected/views/visitor/_form_vic.php
@@ -965,7 +965,10 @@ $countryList = CHtml::listData(Country::model()->findAll(), 'id', 'name');
 
                 $('#User_workstation').append('<option value="">Workstation</option>');
                 $.each(r.data, function (index, value) {
-                    $('#User_workstation').append('<option value="' + value.id + '">' + 'Workstation: ' + value.name + '</option>');
+                    var str = '<option ';
+                    if (index == 0) str += 'selected';
+                    str += ' value="' + value.id + '">' + 'Workstation: ' + value.name + '</option>';
+                    $('#User_workstation').append(str);
                 });
                 //$("#User_workstation").val(value);
                 if ($("#currentAction").val() == 'update') {

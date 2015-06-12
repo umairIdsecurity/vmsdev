3bc5f67c5508cd051e755bdd98c8f13d4c0fed81
Tran Pham <tran.pham@ceresolutions.com>
2015-06-12 18:24:03 +0700

    CAVMS-395 - Navigation issue when host search do not return any result

--- protected/views/visitor/create.php
+++ protected/views/visitor/create.php
@@ -163,9 +163,10 @@ $(document).ready(function () {
             showHideTabs('findVisitorB', 'findVisitorA', 'findVisitor', 'selectCardA', 'selectCard', 'findHostA', 'findHost');
         });
 
-        $("#clicktabB").click(function (e) {
+        $(document).on("click", "#clicktabB", function (e) {
             e.preventDefault();
             $(".visitorReason").hide();
+            $(".visitorType").hide();
             if ($("#Visitor_visitor_type").val() == 1 || $("#Visitor_visitor_type_search").val() == 1) {
                 $("#findHostA").html("Add Patient Details");
                 $("#findHostB").html("Add Patient Details");
@@ -175,6 +176,8 @@ $(document).ready(function () {
             }
             showHideTabs('findHostB', 'findHostA', 'findHost', 'findVisitorA', 'findVisitor', 'selectCardA', 'selectCard');
 
+            hidePreviousPage('step2Tab', 'step3Tab');
+            $('#findHost').addClass('active');
         });
 
         $("#clicktabB1").click(function (e) {

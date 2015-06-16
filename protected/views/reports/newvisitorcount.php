7b25c178a146bad549057ae0c5ba3676264b5c9f
Muhammad Asher <developer@discretelogix.com>
2015-06-16 10:32:40 +0500

    removed ASIC AND VIC profile_types and show only CORPORATE profile_type REPORTING CVMS Reports

--- protected/views/reports/newvisitorcount.php
+++ protected/views/reports/newvisitorcount.php
@@ -6,7 +6,6 @@
 
 $rangeRadio = Yii::app()->request->getParam("rangeRadio");
 $weeklyInterval = Yii::app()->request->getParam("weeklyInterval");
-$profileType = Yii::app()->request->getParam("profileType");
 
 $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'visitor-type-filter-form',
@@ -27,15 +26,6 @@ $form=$this->beginWidget('CActiveForm', array(
                     <label class="radio"><input onclick="this.form.submit();" type="radio" value="monthly" name="rangeRadio" checked="checked" /> Monthly</label> &nbsp;&nbsp;
                     <label class="radio"><input onclick="this.form.submit();" type="radio" value="weekly" name="rangeRadio" <?php if(!empty($rangeRadio) && $rangeRadio == "weekly") {echo "checked";}?> /> Weekly</label> &nbsp;&nbsp;
                     <label class="radio"><input onclick="this.form.submit();" type="radio" value="daily" name="rangeRadio" <?php if(!empty($rangeRadio) && $rangeRadio == "daily") {echo "checked";}?> /> Daily</label>
-                    
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <label class="radio">Select Profile Type </label>
-                    <select name="profileType">
-                        <option value="ALL" selected="selected">All</option>
-                        <option value="CORPORATE" <?php if(!empty($profileType) && $profileType == "CORPORATE") {echo "selected";}?> >Corporate</option>
-                        <option value="VIC" <?php if(!empty($profileType) && $profileType == "VIC") {echo "selected";}?> >VIC</option>
-                        <option value="ASIC" <?php if(!empty($profileType) && $profileType == "ASIC") {echo "selected";}?> >ASIC</option>
-                    </select>
                 </div>
         </fieldset>
         

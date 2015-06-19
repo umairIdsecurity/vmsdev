804e82e8e2eb01278dab3d3b30a984ee5052ffcc
dxasher <developer@discretelogix.com>
2015-06-19 12:44:04 +0500

    induction migration

---
+++ protected/migrations/m150619_073834_add_inductions_three_columns_in_user_table.php
@@ -0,0 +1,19 @@
+<?php
+
+class m150619_073834_add_inductions_three_columns_in_user_table extends CDbMigration
+{
+	
+	public function safeUp()
+	{
+             $this->addColumn('user', 'is_required_induction', 'BOOLEAN NULL');
+             $this->addColumn('user', 'is_completed_induction', 'BOOLEAN NULL');
+             $this->addColumn('user', 'induction_expiry', 'DATE NULL');
+	}
+
+	public function safeDown()
+	{
+            $this->addColumn('user', 'is_required_induction', 'BOOLEAN NULL');
+            $this->addColumn('user', 'is_completed_induction', 'BOOLEAN NULL');
+            $this->addColumn('user', 'induction_expiry', 'DATE NULL');
+	}
+}

35a6f9336894c2804e6a5012a14d5867ce3cfaba
Thao Tran <ththao@ceresolutions.com>
2015-06-23 16:31:02 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-541

--- protected/extensions/validator/DropDown.php
+++ protected/extensions/validator/DropDown.php
@@ -11,11 +11,17 @@ class DropDown extends CValidator
     {
         if ($object->profile_type == Visitor::PROFILE_TYPE_VIC) {
             if ($object->$attribute == '') {
-                $this->addError($object, $attribute, 'Please Select a ' . $object->getAttributeLabel($attribute));
+                $this->addError($object, $attribute, 'Please select a ' . $object->getAttributeLabel($attribute));
+            }
+        } elseif ($object->profile_type == Visitor::PROFILE_TYPE_ASIC) {
+            if ($attribute == 'company' || $attribute == 'visitor_card_status' || $attribute == 'asic_expiry') {
+                if ($object->$attribute == '') {
+                    $this->addError($object, $attribute, 'Please select a ' . $object->getAttributeLabel($attribute));
+                }
             }
         } else {
             if ($attribute == 'company' && $object->$attribute == '') {
-                $this->addError($object, $attribute, 'Please Select a ' . $object->getAttributeLabel($attribute));
+                $this->addError($object, $attribute, 'Please select a ' . $object->getAttributeLabel($attribute));
             }
         }
 
@@ -26,13 +32,21 @@ class DropDown extends CValidator
         if ($object->profile_type == Visitor::PROFILE_TYPE_VIC) {
             return "
             if(value == '') {
-                 messages.push(" . CJSON::encode('Please Select a ' . $object->getAttributeLabel($attribute)) . ");
+                 messages.push(" . CJSON::encode('Please select a ' . $object->getAttributeLabel($attribute)) . ");
             }";
+        } elseif ($object->profile_type == Visitor::PROFILE_TYPE_ASIC) {
+            if ($attribute == 'company' || $attribute == 'visitor_card_status' || $attribute == 'asic_expiry') {
+                return "
+            if(value == '') {
+                 messages.push(" . CJSON::encode('Please select a ' . $object->getAttributeLabel($attribute)) . ");
+            }";
+            }
+
         } else {
             if ($attribute == 'company') {
                 return "
             if(value == '') {
-                 messages.push(" . CJSON::encode('Please Select a ' . $object->getAttributeLabel($attribute)) . ");
+                 messages.push(" . CJSON::encode('Please select a ' . $object->getAttributeLabel($attribute)) . ");
             }";
             }
         }

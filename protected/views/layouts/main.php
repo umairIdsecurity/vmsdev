93dc6021abca989c53b6e496db41b9c495190344
Thao Tran <ththao@ceresolutions.com>
2015-07-02 14:28:31 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-578

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -41,6 +41,8 @@ $userRole = $session['role'];
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->controller->assetsBase; ?>/css/imgareaselect-default.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->controller->assetsBase; ?>/css/gridview.css" />
         <?php
+        $session['tenant'] = $session['id'];
+
         $tenantCompany = User::model()->findByPk($session['tenant'])->company;
         $company = Company::model()->findByPk($tenantCompany);
 

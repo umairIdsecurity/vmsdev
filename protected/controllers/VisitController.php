89e16360f18770af05b1970b1357b371b0ed7032
Thao Tran <ththao@ceresolutions.com>
2015-07-03 16:26:37 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-604

--- protected/controllers/VisitController.php
+++ protected/controllers/VisitController.php
@@ -894,8 +894,25 @@ class VisitController extends Controller {
                     break;
             }
         }
-
-        
+        if(isset($_POST['AddAsicEscort'])) {
+            $asicEscort = new Visitor();
+            $visitorService = new VisitorServiceImpl();
+            $asicEscort->attributes = $_POST['AddAsicEscort'];
+            $asicEscort->profile_type = Visitor::PROFILE_TYPE_ASIC;
+            $asicEscort->visitor_card_status = 6;
+            $asicEscort->escort_flag = 1;
+            $asicEscort->date_created = date("Y-m-d H:i:s");
+            $asicEscort->company = Yii::app()->user->tenant;
+            $asicEscort->tenant = Yii::app()->user->tenant;
+            if (empty($asicEscort->visitor_workstation)) {
+                $asicEscort->visitor_workstation = $session['workstation'];
+            }
+            if ($result = $visitorService->save($asicEscort, NULL, $session['id'])) {
+                $model->asic_escort = $asicEscort->id;
+            } else {
+                Yii::app()->end();
+            }
+        }
 
         if ($visitService->save($model, $session['id'])) {
             echo $model->id;

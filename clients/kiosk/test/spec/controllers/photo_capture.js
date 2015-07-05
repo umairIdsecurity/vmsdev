447643aa4961a4bbe7cdacbd597044df9a465442
Geoff Stewart <geoffstewart@Geoffs-MacBook-Pro.local>
2015-07-05 14:42:31 +0800

    Reorg kiosk codebase

---
+++ clients/kiosk/test/spec/controllers/photo_capture.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: PhotoCaptureCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('kioskApp'));
+
+  var PhotoCaptureCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    PhotoCaptureCtrl = $controller('PhotoCaptureCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});

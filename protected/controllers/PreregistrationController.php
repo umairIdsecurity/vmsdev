bcacbfaee6cc89f8d02c268a43890ad84ccf02cd
streetcoder <proshimul@yahoo.com>
2015-07-06 14:55:19 +0600

    CAVMS-598

--- protected/controllers/PreregistrationController.php
+++ protected/controllers/PreregistrationController.php
@@ -248,13 +248,25 @@ class PreregistrationController extends Controller
 			$model->profile_type = 'ASIC';
 			$model->attributes = $_POST['Registration'];
 			if ($model->save()) {
-				$to = 'proshimul@yahoo.com';
+				/*$to = 'proshimul@yahoo.com';
 				$subject = 'Test email';
-				$body = 'Test email';
+				$body = 'Test email';*/
 
+				$loggedUserEmail = 'shimulcsc@yahoo.com';
+				$headers = "MIME-Version: 1.0" . "\r\n";
+				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+				$headers .= "From: ".$loggedUserEmail."\r\nReply-To: ".$loggedUserEmail;
+				$to=$model->email;
+				$subject="Preregistration email notification";
+				$body = "<html><body>Hi,<br><br>".
+					"This is preregistration email.<br><br>".
+					"Please click on the below URL:<br>".
+					"http://vmsprdev.identitysecurity.info/index.php/preregistration<br>";
 				$body .="<br>"."Thanks,"."<br>Admin</body></html>";
-				mail($to, $subject, $body);
+				mail($to, $subject, $body, $headers);
 
+
+				//mail($to, $subject, $body);
 			}
 			//print_r($model->getErrors());
 

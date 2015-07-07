56182519289e930002f1b86a958e84a14f46cf29
ntdat <ntdat@ceresolutions.com>
2015-07-07 14:52:07 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-102 - VIC Issuing Print File

--- protected/views/cardGenerated/printpdf.php
+++ protected/views/cardGenerated/printpdf.php
@@ -4,6 +4,7 @@ if (array_key_exists($model->card_type, CardType::$CARD_TYPE_LIST)) {
 }
 
 $visitorName = ($visitorModel->first_name) ? $visitorModel->first_name : "" . ' ' . ($visitorModel->last_name) ? $visitorModel->last_name : "";
+
 if (strlen($visitorModel->first_name . ' ' . $visitorModel->last_name) > 32) {
     $first_name = explode(' ', $visitorModel->first_name);
     $last_name = explode(' ', $visitorModel->last_name);
@@ -21,7 +22,13 @@ if ($company) {
     throw new CHttpException(404, 'Company not found for this User.');
 }
 
+$image = Photo::model()->findByPk($visitorModel->company);
+
 $companyLogo =  Photo::model()->getAbsolutePathOfImage(Photo::COMPANY_IMAGE, $visitorModel->company);
+
+if ($companyLogo  == Photo::model()->defaultAbsoluteImage() ){
+    $companyLogo = null;
+}
 $userPhoto = Photo::model()->getAbsolutePathOfImage(Photo::VISITOR_IMAGE,$model->visitor);
 
 $card = CardGenerated::model()->findByPk($model->card);
@@ -35,8 +42,12 @@ $visitorName = wordwrap($visitorName, 13, "\n", true);
 
 $dateExpiry = date('dMy');
 if ($model->card_type != CardType::SAME_DAY_VISITOR) {
-    $dateExpiry = date("dMy", strtotime($model->date_out));
+    $dateExpiry = date("dMy", strtotime($model->date_check_out));
 }
+
+$first_name = $visitorModel->first_name != "" ? $visitorModel->first_name : "N/A";
+$last_name = $visitorModel->last_name != "" ? $visitorModel->last_name : "N/A";
+$cardCode = $cardCode != "" ? $cardCode : "N/A";
 
 //if ($model->time_check_out && $model->card_type == CardType::VIC_CARD_24HOURS && $model->visit_status == VisitStatus::ACTIVE) {
 //$dateExpiry.="<br>" . substr($model->time_check_out, 0, -3);
@@ -67,114 +78,181 @@ if ($model->card_type != CardType::VIC_CARD_MANUAL) {
 ?>
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Untitled Document</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>Untitled Document</title>
+</head>
+<style>
+    .card-print {
+        width:256px;
+        border:1px solid #000;
+        height:405px;
+        border-radius:20px;
+        background:<?= $bgcolor; ?>;
+        position: relative;
+        float: left;
+    }
 
-    <body style="font-family:Arial, sans-serif;">
-        <!--Demo 1 Start-->
-        <?php if ($type == 1) { ?>
-            <table>
+    .card-print .img-visitor {
+        width:135px;
+        height:180px;
+        background:#fff;
+        margin-left:15px;
+        margin-top:13px;
+        border:1px solid #000;
+    }
+    .card-print .img-visitor img {
+        width:135px;
+        height:180px;
+        border: none;
+    }
+
+    .card-date-text {
+        font-size: 30px;
+        font-weight: bold;
+        text-align: left;
+        margin-bottom: 8px;
+        margin-left: 5px;
+        line-height: 32px;
+        margin-top: 3px;
+    }
+
+    .card-date-text span {
+        margin-top:-8px;
+        font-size: 60px;
+        vertical-align: top;
+    }
+
+    .card-info {
+        text-align:center; line-height:20px;  margin:10px 0 0 5px; color:#000;
+    }
+
+    .card-visit-info {
+        font-size:22px;
+        width:256px;
+        float:left;
+        font-weight:bold;
+        line-height:24px;
+        margin:5px 0 3px 0;
+        text-transform: capitalize;
+    }
+
+    .text-cmp {
+        font-size:25px; font-weight:bold; margin:0 0 10px 0;
+    }
+    .card-footer{
+        background:#fff;
+        border-bottom-right-radius: 20px;
+        border-bottom-left-radius: 20px;
+        width:258px; height:40px;
+        padding-top: 8px;
+        position: absolute;
+        bottom: 1px; left: 1px;
+    }
+    .card-footer .img-logo {
+        width:69px;
+        height:30px;
+        margin-left:15px;
+        margin-top:2px;
+        display:inline-block;
+    }
+    .img-logo img {
+        width: auto;
+        height: 30px;
+    }
+    .card-text {
+        width:256px;
+        border:1px solid #000;
+        height:410px;
+        border-radius:20px;
+        background:#fff;
+        float: left;
+    }
+
+    .card-style-3  .card-print {
+        background-color: #FFFFFF;
+    }
+</style>
+<body style="font-family:Arial, sans-serif;">
+<div class="card-style-<?=$type?>">
+    <?php
+    if($type == 1) {
+        ?>
+        <table>
+            <tbody>
                 <tr>
                     <td>
-                        <div style="float:left; width:256px; border:1px solid #000; height:405px; border-radius:20px; background:<?= $bgcolor; ?>;">
-                            <div style="width:150px; height:200px; background:#fff; margin-left:15px; margin-top:13px;">
-                                <img border="0" style="width:150px; height:200px;" src="<?= $userPhoto; ?>">
+                        <!--Box 1-->
+                        <div class="card-print">
+                            <div class="img-visitor">
+                                <img src="<?= $userPhoto; ?>">
                             </div>
-                            <div style=" text-align:center; line-height:20px;  margin:10px 0 0 5px; color:#000;">
-                                <p style="font-size:25px; font-weight:bold; margin:0 0 10px 0;"><?= $companyCode; ?></p>
-
-                                <strong style="font-size: 40px;font-weight: bold; text-align: left; width: 100%; float: left; margin-bottom: 13px; margin-left: 5px; line-height: 32px; margin-top: 3px;">
-                                    <span style=" margin-top:-8px; font-size: 60px; vertical-align: top; "><?php echo($model->card_type == CardType::CONTRACTOR_VISITOR) ? 'C' : 'V'; ?></span>
-                                    <?= $dateExpiry ?>
-                                </strong>
-                                <p style="font-size:25px; width:256px; float:left; display: inline-block;  font-weight:bold; line-height:20.9px; margin:5px 0 3px 0;">
-                                    <?= strtoupper(($visitorModel->first_name != "") ? $visitorModel->first_name : "N/A") ?><br>
-                                    <?= strtoupper(($visitorModel->last_name != "") ? $visitorModel->last_name : "N/A") ?><br>
-                                    <?= strtoupper(($cardCode != "") ? $cardCode : "N/A") ?></p>
+                            <div class="card-info">
+                                <p class="text-cmp"><?= $companyCode; ?></p>
+                                <p class="card-date-text">
+                                    <span> <?php echo($model->card_type == CardType::CONTRACTOR_VISITOR) ? 'C' : 'V'; ?> </span><?= $dateExpiry ?>
+                                </p>
+                                <p class="card-visit-info"><?=$first_name ?><br><?= $last_name ?><br><?= $cardCode ?></p>
                             </div>
-                            <div style="background:#fff; border-bottom-right-radius: 20px;border-bottom-left-radius: 20px; width:260px; height:48.7px;">
-                                <div style="width:69px;  height:30px; margin-left:15px; margin-top:2px; display:inline-block;">
-                                    <img border="0" style="height:30x; width:100%;" src="<?= $companyLogo; ?>">
+                            <?php
+                            if($companyLogo) {
+                                ?>
+                                <div class="card-footer">
+                                    <div class="img-logo">
+                                        <img src="<?= $companyLogo; ?>">
+                                    </div>
                                 </div>
-                            </div>
+                            <?php
+                            }
+                            ?>
                         </div>
                     </td>
                     <td>
-                        <div style=" float:left; width:256px; border:1px solid #000; height:410px; border-radius:20px; background:#fff;">
+                        <!--Box 2-->
+                        <div class="card-text">
                             <div style="font-size: 20px; padding: 10px; text-align: center;"><?= $backText ?></div>
                         </div>
                     </td>
-
                 </tr>
-            </table>
-        <?php } else if ($type == 2) {
-            ?>
-            <div style="width:100%; float:left;">
-                <!--Box 1-->
-                <div style="float:left; width:256px; border:1px solid #000; height:405px; border-radius:20px; background:<?= $bgcolor; ?>;">
-                    <div style="width:150px; height:200px; background:#fff; margin-left:15px; margin-top:13px; border:1px solid #000;">
-                        <img border="0" style="width:150px; height:200px;" src="<?= $userPhoto; ?>">
-                    </div>
-                    <div style=" text-align:center; line-height:20px;  margin:10px 0 0 5px; color:#000;">
-                        <p style="font-size:25px; font-weight:bold; margin:0 0 10px 0;"><?= $companyCode; ?></p>
+            </tbody>
 
-                        <strong style="font-size: 40px;font-weight: bold; text-align: left; width: 100%; float: left; margin-bottom: 13px; margin-left: 5px; line-height: 32px; margin-top: 3px;">
-                            <span style=" margin-top:-8px; font-size: 60px; vertical-align: top; "><?php echo($model->card_type == CardType::CONTRACTOR_VISITOR) ? 'C' : 'V'; ?></span>
-                            <?= $dateExpiry ?>
-                        </strong>
-                        <p style="font-size:25px; width:256px; float:left; display: inline-block;  font-weight:bold; line-height:20.9px; margin:5px 0 3px 0;">
-                            <?= strtoupper(($visitorModel->first_name != "") ? $visitorModel->first_name : "N/A") ?><br>
-                            <?= strtoupper(($visitorModel->last_name != "") ? $visitorModel->last_name : "N/A") ?><br>
-                            <?= strtoupper(($cardCode != "") ? $cardCode : "N/A") ?></p>
-                    </div>
-                    <div style="background:#fff; border-bottom-right-radius: 20px;border-bottom-left-radius: 20px; width:258px; height:48.7px; position: absolute; bottom: 0; left: 1px;">
-                        <div style="width:69px;  height:30px; margin-left:15px; margin-top:2px; display:inline-block; border:1px solid #000;">
-                            <img border="1" style="height:30x; width:100%;" src="<?= $companyLogo; ?>">
-                        </div>
-                    </div>
-                </div>
-                <!--Box 2-->
-                <div style=" float:left; width:256px; border:1px solid #000; height:410px; border-radius:20px; background:#fff;">
-                    <div style="font-size: 20px; padding: 10px; text-align: center;"><?= $backText ?></div>
+        </table>
+    <?php
+    } else {
+        ?>
+        <!--Box 1-->
+        <div class="card-print">
+            <div class="img-visitor">
+                <img src="<?= $userPhoto; ?>">
+            </div>
+            <div class="card-info">
+                <p class="text-cmp"><?= $companyCode; ?></p>
+                <p class="card-date-text">
+                    <span> <?php echo($model->card_type == CardType::CONTRACTOR_VISITOR) ? 'C' : 'V'; ?> </span><?= $dateExpiry ?>
+                </p>
+                <p class="card-visit-info"><?=$first_name ?><br><?= $last_name ?><br><?= $cardCode ?></p>
+            </div>
+            <?php
+            if($companyLogo) {
+                ?>
+            <div class="card-footer">
+                <div class="img-logo">
+                    <img src="<?= $companyLogo; ?>">
                 </div>
             </div>
             <?php
-        } elseif ($type == 3) {?>
-             <div style="width:100%; float:left;">
-                <!--Box 1-->
-                <div style="float:left; width:256px; border:1px solid #000; height:405px; border-radius:20px;">
-                    <div style="width:150px; height:200px; background:#fff; margin-left:15px; margin-top:13px; border:1px solid #000;">
-                        <img border="0" style="width:150px; height:200px;" src="<?= $userPhoto; ?>">
-                    </div>
-                    <div style=" text-align:center; line-height:20px;  margin:10px 0 0 5px; color:#000;">
-                        <p style="font-size:25px; font-weight:bold; margin:0 0 10px 0;"><?= $companyCode; ?></p>
+            }
+            ?>
+        </div>
+        <!--Box 2-->
+        <div class="card-text">
+            <div style="font-size: 20px; padding: 10px; text-align: center;"><?= $backText ?></div>
+        </div>
+    <?php
+    }
+    ?>
 
-                        <strong style="font-size: 40px;font-weight: bold; text-align: left; width: 100%; float: left; margin-bottom: 13px; margin-left: 5px; line-height: 32px; margin-top: 3px;">
-                            <span style=" margin-top:-8px; font-size: 60px; vertical-align: top; "><?php echo($model->card_type == CardType::CONTRACTOR_VISITOR) ? 'C' : 'V'; ?></span>
-                            <?= $dateExpiry ?>
-                        </strong>
-                        <p style="font-size:25px; width:256px; float:left; display: inline-block;  font-weight:bold; line-height:20.9px; margin:5px 0 3px 0;">
-                            <?= strtoupper(($visitorModel->first_name != "") ? $visitorModel->first_name : "N/A") ?><br>
-                            <?= strtoupper(($visitorModel->last_name != "") ? $visitorModel->last_name : "N/A") ?><br>
-                            <?= strtoupper(($cardCode != "") ? $cardCode : "N/A") ?></p>
-                    </div>
-                    <div style="background:#fff; border-bottom-right-radius: 20px;border-bottom-left-radius: 20px; width:260px; height:48.7px;">
-                        <div style="width:69px;  height:30px; margin-left:15px; margin-top:2px; display:inline-block; border:1px solid #000;">
-                            <img border="1" style="height:30x; width:100%;" src="<?= $companyLogo; ?>">
-                        </div>
-                    </div>
-                </div>
-                <!--Box 2-->
-                <div style=" float:left; width:256px; border:1px solid #000; height:410px; border-radius:20px; background:#fff;">
-                    <div style="font-size: 20px; padding: 10px; text-align: center;"><?= $backText ?></div>
-                </div>
-            </div>
-        <?php }
-        ?>
+</div>
 
-
-
-    </body>
+</body>
 </html>

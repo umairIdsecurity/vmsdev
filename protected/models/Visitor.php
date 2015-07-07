d3f44023808d172f4dbec6d14db2e12eb4b2f19c
ntdat <ntdat@ceresolutions.com>
2015-07-07 16:14:28 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-398 - Please correct spelling of From Driver's License To Drivers Licence

--- protected/models/Visitor.php
+++ protected/models/Visitor.php
@@ -99,7 +99,7 @@ class Visitor extends CActiveRecord {
 
     public static $IDENTIFICATION_TYPE_LIST = array(
         'PASSPORT'        => 'Passport',
-        'DRIVERS_LICENSE' => 'Drivers Licence',
+        'DRIVERS_LICENSE' => 'Driver Licence',
         'PROOF_OF_AGE'    => 'Proof of Age Card',
     );
 

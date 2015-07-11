ff6d3817071d296a06e1479b2bce6be3428b49f5
streetcoder <proshimul@yahoo.com>
2015-07-11 14:11:20 +0600

    CAVMS-682

--- protected/models/preregistration/Registration.php
+++ protected/models/preregistration/Registration.php
@@ -55,6 +55,7 @@ class Registration extends CActiveRecord {
     public $alternative_identification;
     public $companycode;
     public $is_asic_verification;
+    public $selected_asic_id;
 
     const PROFILE_TYPE_CORPORATE = 'CORPORATE';
     const PROFILE_TYPE_VIC = 'VIC';
@@ -217,7 +218,7 @@ class Registration extends CActiveRecord {
 			array('contact_street_type', 'length', 'max'=>8),
 			array('contact_state', 'length', 'max'=>3),
 			array('contact_postcode', 'length', 'max'=>10),
-			array('date_of_birth, notes, identification_document_expiry, identification_alternate_document_expiry1, identification_alternate_document_expiry2, asic_expiry,key_string', 'safe'),
+			array('date_of_birth, notes, identification_document_expiry, identification_alternate_document_expiry1, identification_alternate_document_expiry2, asic_expiry,key_string , selected_asic_id', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, first_name, middle_name, last_name, email, contact_number, date_of_birth, company, department, position, staff_id, notes, password, role, visitor_type, visitor_status, vehicle, photo, created_by, is_deleted, tenant, tenant_agent, visitor_card_status, visitor_workstation, profile_type, identification_type, identification_country_issued, identification_document_no, identification_document_expiry, identification_alternate_document_name1, identification_alternate_document_no1, identification_alternate_document_expiry1, identification_alternate_document_name2, identification_alternate_document_no2, identification_alternate_document_expiry2, contact_unit, contact_street_no, contact_street_name, contact_street_type, contact_suburb, contact_state, contact_country, asic_no, asic_expiry, verifiable_signature, contact_postcode, date_created', 'safe', 'on'=>'search'),

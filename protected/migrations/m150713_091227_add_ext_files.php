c5212e98235065d92733308b0fb7a0a0bda3a4db
Dat Nguyen <ntdat@ceresolutions.com>
2015-07-13 18:09:52 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-695 - Update migrate for table File, Folder

--- protected/migrations/m150713_091227_add_ext_files.php
+++ protected/migrations/m150713_091227_add_ext_files.php
@@ -8,12 +8,12 @@ class m150713_091227_add_ext_files extends CDbMigration
 	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
-		$this->addColumn('files', 'ext', 'VARCHAR(10) AFTER `file`');
+		$this->addColumn('files', 'ext', 'VARCHAR(10)');
 	}
 
 	public function safeDown()
 	{
-		$this->addColumn('files', 'ext', 'VARCHAR(10) AFTER `file`');
+		$this->dropColumn('files', 'ext');
 	}
 
 }

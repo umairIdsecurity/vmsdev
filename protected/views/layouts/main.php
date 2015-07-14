3e9ee0f207b2f92a1b7f97263859d94e2a627722
dxasher <developer@discretelogix.com>
2015-07-14 11:35:07 +0500

    administration menu change

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -185,7 +185,7 @@ $userRole = $session['role'];
                             <li class="<?php echo ($session['lastPage'] != 'dashboard' && ($this->action->id == "admin" || ($this->id == 'visit' && $this->action->id != 'view') || $this->id == "user" || $this->id == "visitor" || $this->id == "company" || $this->id == "workstation" || $this->id == "visitReason" || $this->id == "companyLafPreferences")) ? "active" : "" ?>">
                                 <?php if (in_array($session['role'], array(Roles::ROLE_ADMIN,Roles::ROLE_AGENT_ADMIN,Roles::ROLE_SUPERADMIN,Roles::ROLE_AGENT_AIRPORT_ADMIN, Roles::ROLE_ISSUING_BODY_ADMIN))) {
                                 ?>
-                                    <a href="<?php echo Yii::app()->createUrl("/user/admin"); ?>">Administration</a>
+                                <a href="<?php echo Yii::app()->createUrl("/user/admin&vms=".strtolower(CHelper::get_allowed_module())); ?>">Administration</a>
                                 <?php }else{ ?>
                                     <p style="width:230px;"></p>
                                 <?php } ?>

1d571f062ae6cef0996161ac9766094250dfff6b
Thao Tran <ththao@ceresolutions.com>
2015-07-14 16:46:01 +0700

    https://ids-jira.atlassian.net/browse/CAVMS-602

--- protected/controllers/VisitController.php
+++ protected/controllers/VisitController.php
@@ -139,20 +139,7 @@ class VisitController extends Controller {
                 if(isset($_POST['Visit']['sendMail']) && $_POST['Visit']['sendMail'] == 'true' ){
                     $visitor = Visitor::model()->findByPk($model->visitor);
                     $host = Visitor::model()->findByPk($model->host);
-                    $mail = new YiiMailMessage;
-                    $mail->from = 'notify.vms@gmail.com';
-                    $mail->addTo($host->email);
-                    $mail->subject = 'Request for verification of VIC profile ';
-                    $param = '<h3>Hi,</h3>';
-                    $param.= '<h3>VIC Holder urgently requires your Verification of their visit.</h3>';
-                    $baseUrl = Yii::app()->getBaseUrl(true);
-                    $vicProfile = Yii::app()->createUrl('visitor/update',array('id'=>$visitor->id));
-                    $link = $baseUrl.$vicProfile;
-                    $param.='Link of VIC Profile: '.$link;
-                    $param.='<h3>Thanks,</h3>';
-                    $param.='<h3>Admin</h3>';
-                    $mail->setBody($param, 'text/html');
-                    Yii::app()->mail->send($mail);
+                    $this->renderPartial('_email_asic_verify',array('visitor'=>$visitor,'host'=>$host));
                 }
                 $this->redirect(array('visit/detail', 'id' => $model->id));
             }
@@ -238,20 +225,7 @@ class VisitController extends Controller {
                     if(isset($_POST['Visit']['sendMail']) && $_POST['Visit']['sendMail'] == 'true' ){
                         $visitor = Visitor::model()->findByPk($model->visitor);
                         $host = Visitor::model()->findByPk($model->host);
-                        $mail = new YiiMailMessage;
-                        $mail->from = 'notify.vms@gmail.com';
-                        $mail->addTo($host->email);
-                        $mail->subject = 'Request for verification of VIC profile ';
-                        $param = '<h3>Hi,</h3>';
-                        $param.= '<h3>VIC Holder urgently requires your Verification of their visit.</h3>';
-                        $baseUrl = Yii::app()->getBaseUrl(true);
-                        $vicProfile = Yii::app()->createUrl('visitor/update',array('id'=>$visitor->id));
-                        $link = $baseUrl.$vicProfile;
-                        $param.='Link of VIC Profile: '.$link;
-                        $param.='<h3>Thanks,</h3>';
-                        $param.='<h3>Admin</h3>';
-                        $mail->setBody($param, 'text/html');
-                        Yii::app()->mail->send($mail);
+                        $this->renderPartial('_email_asic_verify',array('visitor'=>$visitor,'host'=>$host));
                     }
                 }
                 $this->redirect(array('visit/detail', 'id' => $id));

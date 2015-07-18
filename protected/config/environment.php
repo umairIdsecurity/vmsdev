fb4c3fe9a50d931a401e8e8ec1ec431033edea96
Dat nguyen <ntdat@ceresolutions.com>
2015-07-18 15:16:11 +0700

    fix enviroment

--- protected/config/environment.php
+++ protected/config/environment.php
@@ -19,8 +19,8 @@ if (strpos($_SERVER['SERVER_NAME'],'vmsuitest-win') !== false) {
         'components' => array(
             'db' => array(
                 'connectionString' => 'mysql:host=localhost;dbname=vms',
-                'username' => 'root',
-                'password' => '',
+                'username' => 'user_vms',
+                'password' => 'HFz7c9dHrmPqwNGr',
             )
         )
     );

e9e4a362d886efc13ec03dd1a3978b5ee91186b8
julie.stewart@idsecurity.com.au <julie.stewart@idsecurity.com.au>
2015-08-06 21:56:34 +0800

    fixed error with migration that is not compatible with SQL Server

--- protected/migrations/m150804_065337_create_visitor_password_change_request.php
+++ protected/migrations/m150804_065337_create_visitor_password_change_request.php
@@ -2,24 +2,24 @@
 
 class m150804_065337_create_visitor_password_change_request extends CDbMigration
 {
-	public function up()
+	public function safeUp()
 	{
-        $this->execute("CREATE TABLE visitor_password_change_request (
-            id int(11)  NOT NULL AUTO_INCREMENT,
-            visitor_id bigint(20) NOT NULL DEFAULT '0',
-            hash varchar(255) NOT NULL DEFAULT '',
-            created_at DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-            is_used enum('YES','NO') NOT NULL DEFAULT 'NO',
-            PRIMARY KEY (id),
-            UNIQUE KEY (hash),
-            KEY visitor_id (visitor_id),
-            CONSTRAINT visitor_password_change_request_visitor_id FOREIGN KEY (visitor_id) REFERENCES visitor (id) ON DELETE CASCADE ON UPDATE CASCADE
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+
+		$this->createTable('visitor_password_change_request', array(
+			'id' => 'pk',
+			'visitor_id' => 'BIGINT NOT NULL',
+			'hash' => 'VARCHAR(255) NOT NULL',
+			'created_at' => 'DATETIME NOT NULL',
+			'is_used' => 'BIT'
+		));
+
+		$this->addForeignKey('visitor_password_change_request_visitor_id', 'visitor_password_change_request', 'visitor_id', 'visitor', 'id');
+
 	}
 
-	public function down()
+	public function safeDown()
 	{
-        $this->execute("DROP TABLE IF EXISTS visitor_password_change_request");
+		$this->dropTable('visitor_password_change_request');
 	}
 
 	/*

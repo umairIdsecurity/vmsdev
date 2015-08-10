f927597a27ecb17d1da0d8f87823cc93d46f3b72
dxasher <developer@discretelogix.com>
2015-08-10 16:39:32 +0500

    Photo upload issues on windows dev

--- protected/migrations/m150810_051206_alert_fields_in_user_table.php
+++ protected/migrations/m150810_051206_alert_fields_in_user_table.php
@@ -1,6 +1,6 @@
 <?php
 
-class m150810_051205_alert_fields_in_user_table extends CDbMigration
+class m150810_051206_alert_fields_in_user_table extends CDbMigration
 {
 	 
 	// Use safeUp/safeDown to do migration with transaction
@@ -9,6 +9,11 @@ class m150810_051205_alert_fields_in_user_table extends CDbMigration
               $this->alterColumn("user", "photo", "BIGINT NULL");
               $this->alterColumn("user", "timezone_id", "BIGINT NULL");
               $this->alterColumn("user", "user_type", "BIGINT NULL");
+              
+              //Alter Photo field 
+              if ($this->dbConnection->driverName == 'sqlsrv') {
+			$this->alterColumn('photo', 'db_image', 'NVARCHAR(MAX) NULL');
+		}
 	}
 
 	public function safeDown()

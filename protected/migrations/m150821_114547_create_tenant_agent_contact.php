03fd81abd76dfdd02c828a1fb85765cd9f59bfa4
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-08-22 12:51:08 +0800

    yeah nah. problem was with mismatching datatypes in foreign key definitions

--- protected/migrations/m150821_114547_create_tenant_agent_contact.php
+++ protected/migrations/m150821_114547_create_tenant_agent_contact.php
@@ -11,32 +11,32 @@ class m150821_114547_create_tenant_agent_contact extends CDbMigration
             $table = Yii::app()->db->schema->getTable('tenant_agent_contact');
             if(!isset($table)) {
 
-//                $this->createTable('tenant_agent_contact', array(
-//                    'id' => 'pk',
-//                    'user_id' => 'BIGINT NULL',
-//                    'tenant_id' => 'BIGINT NULL',
-//                    'tenant_agent_id' => 'BIGINT NULL',
-//                ));
-//                $this->addForeignKey('fk_tenant_agent_contact_id','tenant_agent_contact','tenant_agent_id','tenant_agent','id');
-//                $this->addForeignKey('fk_tenant_agent_contact_tenant1','tenant_agent_contact','tenant_id','tenant','id');
+                $this->createTable('tenant_agent_contact', array(
+                    'id' => 'pk',
+                    'user_id' => 'BIGINT NULL',
+                    'tenant_id' => 'BIGINT NOT NULL',
+                    'tenant_agent_id' => 'BIGINT NOT NULL',
+                ));
+                $this->addForeignKey('fk_tenant_agent_contact_id','tenant_agent_contact','tenant_agent_id','tenant_agent','id');
+                $this->addForeignKey('fk_tenant_agent_contact_tenant1','tenant_agent_contact','tenant_id','tenant','id');
 
-            $this->execute("CREATE TABLE tenant_agent_contact (
-                           id BIGINT AUTO_INCREMENT NOT NULL,
-                           user_id BIGINT NULL,
-                           tenant_id BIGINT NULL,
-                           tenant_agent_id BIGINT NULL,
-                           PRIMARY KEY (id),
-
-                            CONSTRAINT fk_tenant_agent_contact_id
-                            FOREIGN KEY (tenant_agent_id)
-                            REFERENCES tenant_agent (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-
-                            CONSTRAINT fk_tenant_agent_contact_tenant1
-                            FOREIGN KEY (tenant_id)
-                            REFERENCES tenant (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-
-                           )
-                        ");
+//            $this->execute("CREATE TABLE tenant_agent_contact (
+//                           id BIGINT AUTO_INCREMENT NOT NULL,
+//                           user_id BIGINT NULL,
+//                           tenant_id BIGINT NULL,
+//                           tenant_agent_id BIGINT NULL,
+//                           PRIMARY KEY (id),
+//
+//                            CONSTRAINT fk_tenant_agent_contact_id
+//                            FOREIGN KEY (tenant_agent_id)
+//                            REFERENCES tenant_agent (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+//
+//                            CONSTRAINT fk_tenant_agent_contact_tenant1
+//                            FOREIGN KEY (tenant_id)
+//                            REFERENCES tenant (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+//
+//                           )
+//                        ");
             }
 	}
 

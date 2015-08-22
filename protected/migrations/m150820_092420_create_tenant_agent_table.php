cce3351927d175d09159b599e9106f7245c468fb
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-08-22 11:53:11 +0800

    changes to migrations causing the build to fail

--- protected/migrations/m150820_092420_create_tenant_agent_table.php
+++ protected/migrations/m150820_092420_create_tenant_agent_table.php
@@ -2,42 +2,57 @@
 
 class m150820_092420_create_tenant_agent_table extends CDbMigration
 {
-	 
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-             
-            $table = Yii::app()->db->schema->getTable('tenant_agent');
-            if(!isset($table)) {
-            $this->execute("CREATE TABLE tenant_agent (
-                           id BIGINT NOT NULL,
-                           user_id BIGINT,
-                           tenant_id BIGINT,
-                           for_module VARCHAR(20),
-                           is_deleted BIGINT,
-                           created_by BIGINT,
-                           date_created DATETIME,
-                           PRIMARY KEY (id, user_id, tenant_id),
-                           
-                            CONSTRAINT fk_tenant_agent_company1
-                            FOREIGN KEY (id)
-                            REFERENCES company (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
-                                                       
-                            CONSTRAINT fk_tenant_agent_tenant1
-                            FOREIGN KEY (tenant_id)
-                            REFERENCES tenant (id) ON DELETE NO ACTION ON UPDATE NO ACTION
-                            
-                           )
-                        ");
-            }
-	}
 
-	public function safeDown()
-	{
-            $table = Yii::app()->db->schema->getTable('tenant_agent');
-            if(isset($table)) {
-                $this->execute("DROP TABLE tenant_agent");
-            }
-	}
-	 
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+
+        $table = Yii::app()->db->schema->getTable('tenant_agent');
+        if(!isset($table)) {
+            $this->createTable('tenant_agent',array(
+                'id'          => 'pk',
+                'user_id'     => 'BIGINT NULL',
+                'tenant_id'   => 'BIGINT NULL',
+                'for_module'  => 'VARCHAR(20)',
+                'tenant_id'   => 'BIGINT NULL',
+                'is_deleted'  => 'BIT',
+                'created_by'  => 'BIGINT',
+                'date_created'=> 'DATETIME'
+
+            ));
+
+            $this->addForeignKey('fk_tenant_agent_company1','tenant_agent','id','company','id');
+            $this->addForeignKey('fk_tenant_agent_tenant1','tenant_agent','tenant_id','tenant','id');
+
+//            $this->execute("CREATE TABLE tenant_agent (
+//                           id BIGINT NOT NULL,
+//                           user_id BIGINT,
+//                           tenant_id BIGINT,
+//                           for_module VARCHAR(20),
+//                           is_deleted BIGINT,
+//                           created_by BIGINT,
+//                           date_created DATETIME,
+//                           PRIMARY KEY (id, user_id, tenant_id),
+//
+//                            CONSTRAINT fk_tenant_agent_company1
+//                            FOREIGN KEY (id)
+//                            REFERENCES company (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+//
+//                            CONSTRAINT fk_tenant_agent_tenant1
+//                            FOREIGN KEY (tenant_id)
+//                            REFERENCES tenant (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+//
+//                           )
+//                        ");
+        }
+    }
+
+    public function safeDown()
+    {
+        $table = Yii::app()->db->schema->getTable('tenant_agent');
+        if(isset($table)) {
+            $this->execute("DROP TABLE tenant_agent");
+        }
+    }
+
 }

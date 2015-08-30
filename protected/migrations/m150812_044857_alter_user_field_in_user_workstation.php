73d090b82b82fd2faf675025195652d5c677b596
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-08-30 10:06:00 +0800

    fixed user workstation migration

--- protected/migrations/m150812_044857_alter_user_field_in_user_workstation.php
+++ protected/migrations/m150812_044857_alter_user_field_in_user_workstation.php
@@ -6,7 +6,8 @@ class m150812_044857_alter_user_field_in_user_workstation extends CDbMigration
 // Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
-            $this->dropForeignKey('user_workstation_ibfk_3', 'user_workstation');
+			$fk = ForeignKeyHelper::getForeignKeyName('user_workstation','user','user','id');
+            $this->dropForeignKey($fk, 'user_workstation');
             $this->renameColumn("user_workstation", "user", "user_id");
             $this->addForeignKey("user_workstation_ibfk_3", "user_workstation", "user_id", "user", "id", null, null);
 	}

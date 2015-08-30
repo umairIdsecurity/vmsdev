aa9315272a6ff9102c53bafc01b1cf46ddd1eb98
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-08-30 11:39:23 +0800

    fixed user workstation migration

--- protected/migrations/m150825_085642_change_help_desk_group_table.php
+++ protected/migrations/m150825_085642_change_help_desk_group_table.php
@@ -6,18 +6,18 @@ class m150825_085642_change_help_desk_group_table extends CDbMigration
 	{
 		$this->addColumn('helpdesk_group', 'is_default_value','BIT');
 
-		$this->createTable('helpdesk_group_web_preregistration', array(
-			'helpdesk_group' => 'BIGINT NOT NULL',
-			'web_preregistration' => 'VARCHAR(20)',
-		));
-		$this->addForeignKey('helpdesk_group_web_preregistration_helpdesk_group_fk', 'helpdesk_group_web_preregistration', 'helpdesk_group', 'helpdesk_group', 'id');
+		//$this->createTable('helpdesk_group_web_preregistration', array(
+		//	'helpdesk_group' => 'BIGINT NOT NULL',
+		//	'web_preregistration' => 'VARCHAR(20)',
+		//));
+		//$this->addForeignKey('helpdesk_group_web_preregistration_helpdesk_group_fk', 'helpdesk_group_web_preregistration', 'helpdesk_group', 'helpdesk_group', 'id');
 
 		$this->createTable('helpdesk_group_user_role', array(
 			'helpdesk_group' => 'BIGINT NOT NULL',
 			'role' => 'BIGINT NOT NULL',
 		));
 
-		$this->addForeignKey('helpdesk_group_user_role_helpdesk_group_fk', 'helpdesk_group_user_role', 'helpdesk_group', 'helpdesk_group', 'id');
+		//$this->addForeignKey('helpdesk_group_user_role_helpdesk_group_fk', 'helpdesk_group_user_role', 'helpdesk_group', 'helpdesk_group', 'id');
 		$this->addForeignKey('helpdesk_group_user_role_role_fk',  'helpdesk_group_user_role', 'role', 'roles', 'id');
 	}
 
@@ -26,8 +26,8 @@ class m150825_085642_change_help_desk_group_table extends CDbMigration
 		$this->dropForeignKey('helpdesk_group_user_role_helpdesk_group_fk', 'helpdesk_group_user_role');
 		$this->dropForeignKey('helpdesk_group_user_role_role_fk', 'helpdesk_group_user_role');
 		$this->dropTable('helpdesk_group_user_role');
-		$this->dropForeignKey('helpdesk_group_web_preregistration_helpdesk_group_fk', 'helpdesk_group_web_preregistration');
-		$this->dropTable('helpdesk_group_web_preregistration');
+		//$this->dropForeignKey('helpdesk_group_web_preregistration_helpdesk_group_fk', 'helpdesk_group_web_preregistration');
+		//$this->dropTable('helpdesk_group_web_preregistration');
 		$this->dropColumn("helpdesk_group","is_default_value");
 	}
 

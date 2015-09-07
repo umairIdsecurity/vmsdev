2ada7c4b1bcf9314998fafb6362ae56c64cf1190
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-09-07 18:54:30 +0800

    clean up and reorg

--- PAPL/frontend/js/app/check-mobile.js
+++
@@ -1,6 +0,0 @@
-/*
-if (Modernizr.touch) {
-    alert('Touch Screen');
-} else {
-    alert('No Touch Screen');
-}*/
--- dbpatch/Issue142.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `vms`.`visit` SET `is_deleted` = '0' WHERE `id` = '13'; 
-UPDATE `vms`.`visit` SET `is_deleted` = '0' WHERE `id` = '14'; 
-UPDATE `vms`.`visitor` SET `is_deleted` = '0' WHERE `id` = '7'; 
-UPDATE `visit` SET `visit_status` = '4' WHERE `id` = '13'; 

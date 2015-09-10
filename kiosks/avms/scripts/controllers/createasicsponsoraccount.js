af3a494b6271c2e52e0b3331afc8167819132d7d
Duong <softdevelop.hd@gmail.com>
2015-09-10 15:36:31 +0700

    AVMS kiosk css coded: asic sponsor login, asic sponsor remote, camera capture, create asic sponsor account, pickup, declarations page

---
+++ kiosks/avms/scripts/controllers/createasicsponsoraccount.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name avmsKioskApp.controller:CreateasicsponsoraccountCtrl
+ * @description
+ * # CreateasicsponsoraccountCtrl
+ * Controller of the avmsKioskApp
+ */
+angular.module('avmsKioskApp')
+  .controller('CreateasicsponsoraccountCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

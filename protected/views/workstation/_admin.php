48e347c5f3b7b657794deac531f5f89c05c7ba18
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-09-21 20:47:15 +0800

    fixed issue with deleting workstations

--- protected/views/workstation/_admin.php
+++ protected/views/workstation/_admin.php
@@ -23,22 +23,7 @@
 
 <?php
 $module = CHelper::get_allowed_module();
-$availableButtons = array(
-    'update' => array(//the name {reply} must be same
-        'label' => 'Edit', // text label of the button
-        'imageUrl' => false, // image URL of the button. If not set or false, a text link is used, The image must be 16X16 pixels
-    )
-);
 
-if(Yii::app()->user->role == Roles::ROLE_SUPERADMIN){
-    $availableButtons[] = array(
-        'delete' => array(//the name {reply} must be same
-            'label' => 'Delete', // text label of the button
-            'imageUrl' => false, // image URL of the button. If not set or false, a text link is used, The image must be 16X16 pixels
-            'visible' => '(($data->id)!= 1)',
-        )
-    );
-}
 
 $this->widget('zii.widgets.grid.CGridView', array(
     'id' => 'workstation-grid',

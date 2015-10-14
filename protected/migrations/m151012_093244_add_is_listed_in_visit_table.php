2750d37df2f897308126cd16751f00ca2da02990
Geoff Stewart <geoffstewart@Geoffs-MacBook-Pro-2.local>
2015-10-14 20:10:15 +0800

    fixed error with migration

--- protected/migrations/m151012_093244_add_is_listed_in_visit_table.php
+++ protected/migrations/m151012_093244_add_is_listed_in_visit_table.php
@@ -5,7 +5,7 @@ class m151012_093244_add_is_listed_in_visit_table extends CDbMigration
 	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
-		  $this->addColumn('visit','is_listed','TINYINT NOT NULL DEFAULT "1"');
+		  $this->addColumn('visit','is_listed','TINYINT NOT NULL DEFAULT 1	');
 	}
 
 	public function safeDown()

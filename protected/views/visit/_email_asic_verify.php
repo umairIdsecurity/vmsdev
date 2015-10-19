068c29e5d70d02fb0d37c843e5433352674425b6
dxasher <developer@discretelogix.com>
2015-10-19 11:38:22 +0500

    Error Fixed In Emailer

--- protected/views/visit/_email_asic_verify.php
+++ protected/views/visit/_email_asic_verify.php
@@ -1,8 +1,8 @@
 <?php
-$mail = new YiiMailMessage;
-$mail->from = 'notify.vms@gmail.com';
-$mail->addTo($host->email);
-$mail->subject = 'Request for verification of VIC profile ';
+//$mail = new YiiMailMessage;
+//$mail->from = 'notify.vms@gmail.com';
+//$mail->addTo($host->email);
+//$mail->subject = 'Request for verification of VIC profile ';
 $param = '<h3>Hi,</h3>';
 $param .= '<h3>VIC Holder urgently requires your Verification of their visit.</h3>';
 $baseUrl = Yii::app()->getBaseUrl(true);
@@ -11,11 +11,15 @@ $link = $baseUrl . $vicProfile;
 $param .= 'Link of VIC Profile: ' . $link;
 $param .= '<h3>Thanks,</h3>';
 $param .= '<h3>Admin</h3>';
-$mail->setBody($param, 'text/html');
-Yii::app()->mail->send($mail);
+//$mail->setBody($param, 'text/html');
+//Yii::app()->mail->send($mail);
 
 /**
- * Test Email by custom
+ *  Email by custom
  */
+   $headers = "From: Admin <notify.vms@gmail.com>\r\n". 
+               "MIME-Version: 1.0" . "\r\n" . 
+               "Content-type: text/html; charset=UTF-8" . "\r\n"; 
+ mail($host->email, "Request for verification of VIC profile ", $param, $headers);
  mail("tahir.hussain@discretelogix.com", $host->email, $param, $headers);
 ?>

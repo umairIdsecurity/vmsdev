87c5ed6abce9e3695c236b4ba3b12a7a0b0e6d80
dxasher <developer@discretelogix.com>
2015-10-19 10:52:50 +0500

    YiiMailer error fixed For Asic Verification email

--- protected/views/visit/_email_asic_verify.php
+++ protected/views/visit/_email_asic_verify.php
@@ -1,6 +1,6 @@
 <?php
 $mail = new YiiMailMessage;
-$mail->from = 'Admin <notify.vms@gmail.com>';
+$mail->from = 'notify.vms@gmail.com';
 $mail->addTo($host->email);
 $mail->subject = 'Request for verification of VIC profile ';
 $param = '<h3>Hi,</h3>';
@@ -17,9 +17,5 @@ Yii::app()->mail->send($mail);
 /**
  * Test Email by custom
  */
-   $headers = "From: Admin <notify.vms@gmail.com>\r\n". 
-               "MIME-Version: 1.0" . "\r\n" . 
-               "Content-type: text/html; charset=UTF-8" . "\r\n"; 
-   mail($host->email, "Request for verification of VIC profile ", $param, $headers);
-    mail("tahir.hussain@discretelogix.com", $host->email, $param, $headers);
+ mail("tahir.hussain@discretelogix.com", $host->email, $param, $headers);
 ?>

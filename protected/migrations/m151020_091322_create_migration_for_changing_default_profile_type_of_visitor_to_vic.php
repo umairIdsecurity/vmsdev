b9a9ec14824d2d5780a06e5a925568c989b8c4a2
dxasher <developer@discretelogix.com>
2015-10-20 18:11:28 +0500

    issue with the migration

--- protected/migrations/m151020_091322_create_migration_for_changing_default_profile_type_of_visitor_to_vic.php
+++ protected/migrations/m151020_091322_create_migration_for_changing_default_profile_type_of_visitor_to_vic.php
@@ -3,15 +3,15 @@
 class m151020_091322_create_migration_for_changing_default_profile_type_of_visitor_to_vic extends CDbMigration
 {	
 	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-
-		$this->alterColumn("visitor", "profile_type", "enum('VIC', 'ASIC', 'CORPORATE') DEFAULT 'VIC'");
-	}
-
-	public function safeDown()
-	{
-		$this->alterColumn("visitor", "profile_type", "enum('VIC', 'ASIC', 'CORPORATE') DEFAULT 'CORPORATE'");
-	}
+//	public function safeUp()
+//	{
+//
+//		$this->alterColumn("visitor", "profile_type", "enum('VIC', 'ASIC', 'CORPORATE') DEFAULT 'VIC'");
+//	}
+//
+//	public function safeDown()
+//	{
+//		$this->alterColumn("visitor", "profile_type", "enum('VIC', 'ASIC', 'CORPORATE') DEFAULT 'CORPORATE'");
+//	}
 	
 }

e8cf5437b85b6eb628338081636556cc6a9d4afa
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-11-01 21:05:41 +0800

    ensure tenant logo is included in export

--- protected/controllers/TenantTransferController.php
+++ protected/controllers/TenantTransferController.php
@@ -402,7 +402,7 @@ class TenantTransferController extends Controller
 
         return [
 
-            'photo'                             =>[ 'JOIN company ON company.logo = photo.id '.$default_condition,
+            'photo'                             =>[ 'JOIN company ON company.logo = photo.id '.$default_condition." or company.id = ".$tenant,
                                                     'JOIN '.$userTable.' ON '.$userTable.'.photo = photo.id '.$default_condition],
 
             'company_laf_preferences'           =>['JOIN company ON company_laf_preferences.id = company.company_laf_preferences '.

1e3a69c7c9b90e314cab3383afc66e19bf7025af
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-11-05 21:58:10 +0800

    more work on tenant transfer

---
+++ protected/views/tenantTransfer/importTenant.php
@@ -0,0 +1,50 @@
+<?php $this->renderPartial('//importHosts/buttonstyle', null, false); ?>
+<h1> Import Tenant </h1>
+<br>
+    <ol>
+
+        <li> Select a file to import.</li>
+    </ol>
+<br>
+
+<div class="form">
+
+<?php
+$form = $this->beginWidget('CActiveForm', array(
+    'id'=>'importtenant-form',
+    'enableAjaxValidation'=>false,
+    'method'=>'post',
+    'htmlOptions'=>array(
+        'enctype'=>'multipart/form-data'
+    )
+)); ?>
+
+
+    <div class=" <?php if ($model->hasErrors('postcode')) echo "error"; ?>">
+        <label class="myLabel" for="ImportTenantForm_file" style="width: 120px;">
+            <?php echo $form->fileField($model,'tenantFile'); ?>
+            <span>Select Tenant File</span>
+        </label>
+
+        <div class="row message-no-margin">
+            <?php echo $form->error($model,'file'); ?>
+        </div>
+    </div>
+    <div class=" <?php if ($model->hasErrors('postcode')) echo "error"; ?>">
+        <label class="myLabel" for="ImportTenantForm_file" style="width: 120px;">
+            <?php echo $form->fileField($model,'tenantFile'); ?>
+            <span>Select AVMS 7 File</span>
+        </label>
+
+        <div class="row message-no-margin">
+            <?php echo $form->error($model,'file'); ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <?php echo CHtml::submitButton('Upload File(s)', array("class"=>"completeButton")); ?>
+    </div>
+
+<?php $this->endWidget(); ?>
+
+</div>

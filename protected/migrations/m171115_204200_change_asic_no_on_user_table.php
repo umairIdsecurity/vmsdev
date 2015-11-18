eaf0fc1551fc1eed4d6b533dee162038e8de569f
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2015-11-18 20:42:59 +0800

    avms7 import

--- protected/migrations/m171115_204200_change_asic_no_on_user_table.php
+++ protected/migrations/m171115_204200_change_asic_no_on_user_table.php
@@ -4,7 +4,7 @@ class m171115_204200_change_asic_no_on_user_table extends CDbMigration
 {
 	public function safeUp()
 	{
-		$this->alterColumn('user', 'asic_no', 'VARCHAR(20)');
+		//$this->alterColumn('user', 'asic_no', 'VARCHAR(20)');
 	}
 
 	public function safeDown()

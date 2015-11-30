043f98d1b12d38f9a8c6f227f634c95117c50a1d
dxasher <developer@gmail.com>
2015-11-30 15:49:18 +0500

    identification_type fixes

--- protected/migrations/m151130_091727_update_visitor_identification_type_from_enum_to_varchar.php
+++ protected/migrations/m151130_091727_update_visitor_identification_type_from_enum_to_varchar.php
@@ -5,7 +5,7 @@ class m151130_091727_update_visitor_identification_type_from_enum_to_varchar ext
 	// Use safeUp/safeDown to do migration with transaction
 	public function safeUp()
 	{
-		$this->alterColumn("visitor", "identification_type", "enum('PASSPORT','DRIVERS_LICENSE','PROOF_OF_AGE','FIRE_ARMS_LICENCE','WORKING_WITH_CHILDREN_CARD','APPROVED_MANAGER','IMMICARD','HIGH_RISK_WORK_LICENCE','SENIORS_CARD') DEFAULT NULL");
+		$this->alterColumn("visitor","identification_type","VARCHAR(150) DEFAULT NULL");
 	}
 
 	public function safeDown()

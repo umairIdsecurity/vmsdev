9be5f0d2ca8aeb21b42789d02d2772c3bed800ed
Geoff Stewart <geoffstewart@Geoffs-MacBook-Pro-2.local>
2015-12-14 22:52:20 +0800

    updated behat config

--- test/tools/install-tools-osx.sh
+++ test/tools/install-tools-osx.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-#curl -sS https://getcomposer.org/installer | php
-#mv composer.phar /usr/local/bin/composer
+rm -Rf ~/.composer
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
 
-composer global require behat/behat behat/mink behat/mink-extension:dev-master behat/mink-goutte-driver:dev-master behat/mink-selenium2-driver:dev-master
+composer global require behat/behat:2.5.*@stable behat/mink:1.5.*@stable behat/mink-extension:1.3.3 behat/mink-goutte-driver:1.0.9 behat/mink-selenium2-driver:1.1.1 behat/mink-zombie-driver:1.1.0
 
 echo "~/.composer/vendor/bin" >> /etc/paths
 

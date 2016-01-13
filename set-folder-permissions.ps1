1777c21bae7f7a18ae1559c4f0ec07ae925590a2
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2016-01-13 21:14:41 +0800

    added powershell to set directory permissions

--- set-folder-permissions.ps1
+++ set-folder-permissions.ps1
@@ -3,17 +3,28 @@ cls
 
 $folders = @('assets','protected\assets')
 
-foreach($folder in $folders){
+function setAccess($folder,$fileSystemRights
+
+
+    if(!(Test-Path $folder)){
+        Write-Host "creating folder $folder"
+        New-Item -ItemType directory -Path $folder
+    }
 
     $identity = "IIS AppPool\$appPoolName"
-    $fileSystemRights = "FullControl"
+    #$fileSystemRights = "FullControl"
     $inheritanceFlags = "ContainerInherit, ObjectInherit"
     $propagationFlags = "None"
     $accessControlType = "Allow"
     $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($identity, $fileSystemRights, $inheritanceFlags, $propagationFlags, $accessControlType)
 
-    Write-Host "setting permissions on folder $folder"
+    Write-Host "setting permissions on folder $folder for app pool $appPoolName"
     $acl = Get-Acl $folder
     $acl.SetAccessRule($rule)
     Set-Acl $folder $acl
 }
+
+setAccess '.' 'Read'
+setAccess 'assets' 'FullControl'
+setAccess 'protected\assets' 'FullControl'
+setAccess 'protected\runtime' 'FullControl'

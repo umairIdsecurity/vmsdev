c63f5b9f5bbf6104bef801e612edb6929125feb5
Geoff Stewart <geoff.stewart@idsecurity.com.au>
2016-03-07 15:28:42 +0800

    fix for create visit

--- protected/controllers/VisitController.php
+++ protected/controllers/VisitController.php
@@ -86,7 +86,9 @@ class VisitController extends Controller {
             $model->attributes = $visitParams;
 
             // other side of nasty hack to transfer visitor id to this controller
-            $model->visitor = $_SESSION['id_of_last_visitor_created'];
+            if(isset($_SESSION['id_of_last_visitor_created'])) {
+                $model->visitor = $_SESSION['id_of_last_visitor_created'];
+            }
 
             // If datepicker is disabled then date check out is empty
             if (!isset($visitParams['date_check_out'])) {
